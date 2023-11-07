c38f389387408b72ab00d29e360a1be218e87bb4
MohamedAklamaash <aklamaash78@gmail.com>
2023-11-07 18:46:42 +0530

    started working on a landing page

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Home/>
+    </div>
+  )
+}
+
+export default App
